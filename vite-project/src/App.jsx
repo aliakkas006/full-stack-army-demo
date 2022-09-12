2ff937c42b010dc96663e4ac19b1667b74430f31
Ali Akkas <aliakkas006@gmail.com>
2022-09-12 07:15:26 +0600

    state-lifting-filtering-searching

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,96 +1,19 @@
-/**
- * Done: handle user input fields
- * Done: handle operations
- * Done: handle a list of histories
- * Done: render history list
- * restore the history
- */
-
 import { useState } from 'react';
-import InputSection from './components/Inputs/InputSection';
-import OperationSection from './components/Operations/OperationSectiion';
-import shortid from 'shortid';
-import HistorySection from './components/History/HistorySection';
-
-/* 
-custom id generator:
-function* generateId() {
-  let id = 1;
-  while (true) yield id++;
-}
-const getId = generateId(); 
-*/
-
-const initialState = {
-  a: 0,
-  b: 0,
-};
+import ContactForm from './components/contact-app/ContactForm';
+import Table from './components/contact-app/Table';
 
 const App = () => {
-  const [inputState, setInputState] = useState({ ...initialState });
-  const [result, setResult] = useState(0);
-  const [histories, setHistories] = useState([]);
-  const [restoredHistory, setRestoredHistory] = useState(null);
+  const [contacts, setContacts] = useState([]);
 
-  const handleInputFields = (e) => {
-    setInputState({
-      ...inputState,
-      [e.target.name]: parseInt(e.target.value),
-    });
-  };
-
-  const handleArithmeticOps = (operation) => {
-    if (!(inputState.a || inputState.b)) return alert('Invalid input!');
-
-    const f = new Function(
-      'operation',
-      `
-        return ${inputState.a} ${operation} ${inputState.b}
-      `
-    );
-
-    const result = f(operation);
-    setResult(result);
-
-    const historyItem = {
-      id: shortid.generate(),
-      inputs: inputState,
-      operation,
-      result,
-      date: new Date(),
-    };
-
-    setHistories([historyItem, ...histories]);
-  };
-
-  const handleClearOps = () => {
-    setInputState({ ...initialState });
-    setResult(0);
-    setHistories([]);
-  };
-
-  const handleRestoreBin = (historyItem) => {
-    setInputState({ ...historyItem.inputs });
-    setResult(historyItem.result);
-    setRestoredHistory(historyItem.id);
+  const getContact = (contact) => {
+    setContacts([].concat(contacts, contact));
   };
 
   return (
-    <div style={{ width: '50%', margin: '0 auto' }}>
-      <h1>Result: {result} </h1>
-
-      <InputSection inputs={inputState} handleInputFields={handleInputFields} />
-
-      <OperationSection
-        handleArithmeticOps={handleArithmeticOps}
-        handleClearOps={handleClearOps}
-      />
-
-      <HistorySection
-        histories={histories}
-        handleRestoreBin={handleRestoreBin}
-        restoredHistory={restoredHistory}
-      />
+    <div>
+      <h1>Contact app</h1>
+      <ContactForm getContact={getContact} />
+      <Table contacts={contacts} />
     </div>
   );
 };
