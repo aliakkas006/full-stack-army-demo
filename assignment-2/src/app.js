f70cf7b9ae7b93b30c0fdc4e69f9269b23704a15
Ali Akkas <aliakkas006@gmail.com>
2023-05-19 10:58:03 +0600

    logger added

--- assignment-2/src/app.js
+++ assignment-2/src/app.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
 import routes from './routes/index.js';
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(routes);
-
-app.get('/health', (_req, res) => {
-  res.send(`<h2>Everything is OK!<h2>`);
-});
+app.use([express.json()], cors(), morgan('dev'), routes);
 
 export default app;
